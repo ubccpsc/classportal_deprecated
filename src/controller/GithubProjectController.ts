4fc6c1e635c9496a04f4c247f483f00eec734891
Reid Holmes (MBA) <rtholmes@users.noreply.github.com>
2016-08-17 22:44:31 -0700

    test the github controller code

--- src/controller/GithubProjectController.ts
+++ src/controller/GithubProjectController.ts
@@ -6,46 +6,119 @@ import Log from "../Util";
 var request = require('request');
 
 // TODO: migrate to rp: https://www.npmjs.com/package/request-promise
-// var rp = require('request-promise-native');
+var rp = require('request-promise-native');
 
 export default class GithubProjectController {
 
-    private GITHUB_AUTH_TOKEN = 'token 8c017236d0429fe33d8aed1ea435e6777aaeab88';
-    private GITHUB_USER_NAME = 'zhihaoli';
+    private GITHUB_AUTH_TOKEN = 'token 9c6e586170923383fe5bec2a295c1c38d80e1221';
+    private GITHUB_USER_NAME = 'rtholmes';
+    // bruce
+    // private GITHUB_AUTH_TOKEN = 'token 8c017236d0429fe33d8aed1ea435e6777aaeab88';
+    // private GITHUB_USER_NAME = 'zhihaoli';
 
     private org = "CS410-2015Fall";
     private db: any;
 
+    /*
+     public createRepo(repoName: string): Promise<string> {
+     let ctx = this;
+
+     Log.info("GithubProjectController::createRepo(..) - start");
+     return new Promise(function (fulfill, reject) {
+
+     request.post(
+     {
+     url: 'https://api.github.com/orgs/' + ctx.org + '/repos',
+     headers: {
+     'Authorization': ctx.GITHUB_AUTH_TOKEN,
+     'User-Agent': ctx.GITHUB_USER_NAME,
+     'Accept': 'application/json'
+     },
+     json: {
+     name: repoName,
+     private: true,
+     auto_init: true,
+     }
+     },
+
+     function (error: any, response: any, body: any) {
+     if (!error && response.statusCode < 400) {
+     let url = body.html_url;
+     Log.info("GithubProjectController::createRepo(..) - success; url: " + url);
+     fulfill(url);
+     } else {
+     Log.error("GithubProjectController::createRepo(..) - ERROR: " + JSON.stringify(body));
+     reject(body);
+     }
+     }
+     );
+     });
+     }
+     */
     public createRepo(repoName: string): Promise<string> {
         let ctx = this;
+
+        Log.info("GithubProjectController::createRepo(..) - start");
         return new Promise(function (fulfill, reject) {
 
-            request.post(
-                {
-                    url: 'https://api.github.com/orgs/' + ctx.org + '/repos',
-                    headers: {
-                        'Authorization': ctx.GITHUB_AUTH_TOKEN,
-                        'User-Agent': ctx.GITHUB_USER_NAME,
-                        'Accept': 'application/json'
-                    },
-                    json: {
-                        name: repoName,
-                        private: true,
-                        auto_init: true,
-                    }
+            var options = {
+                method: 'POST',
+                uri: 'https://api.github.com/orgs/' + ctx.org + '/repos',
+                headers: {
+                    'Authorization': ctx.GITHUB_AUTH_TOKEN,
+                    'User-Agent': ctx.GITHUB_USER_NAME,
+                    'Accept': 'application/json'
                 },
+                body: {
+                    name: repoName,
+                    private: true,
+                    auto_init: true
+                },
+                json: true
+            };
 
-                function (error: any, response: any, body: any) {
-                    if (!error && response.statusCode < 400) {
-                        let url = body.html_url;
-                        Log.info("GithubProjectController::createRepo(..) - success; url: " + url);
-                        fulfill(url);
-                    } else {
-                        Log.error("GithubProjectController::createRepo(..) - ERROR: " + error);
-                        reject(error);
-                    }
+            rp(options).then(function (body: any) {
+                let url = body.html_url;
+                Log.info("GithubProjectController::createRepo(..) - success; url: " + url);
+                fulfill(url);
+            }).catch(function (err: any) {
+                Log.error("GithubProjectController::createRepo(..) - ERROR: " + JSON.stringify(err));
+                reject(err);
+            });
+
+        });
+    }
+
+    /**
+     * Deletes a repo from the organization.
+     *
+     * @param repoName
+     * @returns {Promise<T>}
+     */
+    public deleteRepo(repoName: string): Promise<string> {
+        let ctx = this;
+
+        Log.info("GithubProjectController::deleteRepo(..) - start");
+        return new Promise(function (fulfill, reject) {
+
+            var options = {
+                method: 'DELETE',
+                uri: 'https://api.github.com/repos/' + ctx.org + '/' + repoName,
+                headers: {
+                    'Authorization': ctx.GITHUB_AUTH_TOKEN,
+                    'User-Agent': ctx.GITHUB_USER_NAME,
+                    'Accept': 'application/json'
                 }
-            );
+            };
+
+            rp(options).then(function (body: any) {
+                Log.info("GithubProjectController::deleteRepo(..) - success; body: " + body);
+                fulfill(body);
+            }).catch(function (err: any) {
+                Log.error("GithubProjectController::deleteRepo(..) - ERROR: " + JSON.stringify(err));
+                reject(err);
+            });
+
         });
     }
 
@@ -55,120 +128,163 @@ export default class GithubProjectController {
      * @param groupName
      * @returns {Promise<T>}
      */
-    public createTeam(groupName: string): Promise<string> {
+    public createTeam(groupName: string): Promise<number> {
         let ctx = this;
+
+        Log.info("GithubProjectController::createTeam(..) - start");
         return new Promise(function (fulfill, reject) {
-            request.post(
-                {
-                    url: 'https://api.github.com/orgs/' + ctx.org + '/teams',
+
+
+            var options = {
+                method: 'POST',
+                uri: 'https://api.github.com/orgs/' + ctx.org + '/teams',
+                headers: {
+                    'Authorization': ctx.GITHUB_AUTH_TOKEN,
+                    'User-Agent': ctx.GITHUB_USER_NAME,
+                    'Accept': 'application/json'
+                },
+                body: {
+                    name: groupName
+                },
+                json: true
+            };
+
+            rp(options).then(function (body: any) {
+                let id = body.id;
+                Log.info("GithubProjectController::createTeam(..) - success: " + id);
+                fulfill(id);
+            }).catch(function (err: any) {
+                Log.error("GithubProjectController::createTeam(..) - ERROR: " + err);
+                reject(err);
+            });
+            /*
+             request.post(
+             {
+             url: 'https://api.github.com/orgs/' + ctx.org + '/teams',
+             headers: {
+             'Authorization': ctx.GITHUB_AUTH_TOKEN,
+             'User-Agent': ctx.GITHUB_USER_NAME,
+             'Accept': 'application/json'
+             },
+             json: {
+             name: groupName
+             }
+             },
+
+             function (error: any, response: any, body: any) {
+             if (!error && response.statusCode < 400) {
+             let id = body.id;
+             Log.info("GithubProjectController::createTeam(..) - success: " + id);
+             fulfill(id);
+             // onSuccess(body.id);
+             } else {
+             Log.error("GithubProjectController::createTeam(..) - ERROR: " + response);
+             reject(error);
+             }
+
+             }
+             );
+             */
+        });
+    }
+
+    /**
+     * Deletes a repo from the organization.
+     *
+     * @param repoName
+     * @returns {Promise<T>}
+     */
+    /*
+     public deleteTeam(teamName: string): Promise<string> {
+     let ctx = this;
+
+     Log.info("GithubProjectController::deleteTeam(..) - start");
+     return new Promise(function (fulfill, reject) {
+
+     var options = {
+     method: 'DELETE',
+     uri: 'https://api.github.com/teams/' + teamName,// TODO: needs to be a team id
+     headers: {
+     'Authorization': ctx.GITHUB_AUTH_TOKEN,
+     'User-Agent': ctx.GITHUB_USER_NAME,
+     'Accept': 'application/json'
+     }
+     };
+
+     rp(options).then(function (body: any) {
+     Log.info("GithubProjectController::deleteTeam(..) - success; body: " + body);
+     fulfill(body);
+     }).catch(function (err: any) {
+     Log.error("GithubProjectController::deleteTeam(..) - ERROR: " + JSON.stringify(err));
+     reject(err);
+     });
+
+     });
+     }
+     */
+
+    /**
+     * NOTE: needs the team Id, not the team name!
+     *
+     * @param teamId
+     * @param repoName
+     * @returns {Promise<T>}
+     */
+    public addTeamToRepo(teamId: number, repoName: string) {
+        let ctx = this;
+        Log.info("GithubProjectController::addTeamToRepo( " + teamId + ", " + repoName + " ) - start");
+        return new Promise(function (fulfill, reject) {
+
+            var options = {
+                method: 'PUT',
+                uri: 'https://api.github.com/teams/' + teamId + '/repos/' + ctx.org + '/' + repoName,
+                headers: {
+                    'Authorization': ctx.GITHUB_AUTH_TOKEN,
+                    'User-Agent': ctx.GITHUB_USER_NAME,
+                    'Accept': 'application/json'
+                }
+            };
+
+            rp(options).then(function (body: any) {
+                Log.info("GithubProjectController::addTeamToRepo(..) - success: " + body);
+                // onSuccess(body);
+                fulfill(body);
+            }).catch(function (err: any) {
+                Log.error("GithubProjectController::addTeamToRepo(..) - ERROR: " + err);
+                reject(err);
+            });
+        });
+    }
+
+    public addMembersToTeam(teamId: number, members: string[]) {
+        let ctx = this;
+        Log.info("GithubProjectController::addMembersToTeam(..) - start");
+
+        return new Promise(function (fulfill, reject) {
+            let promises: any = [];
+            for (var member of members) {
+                Log.info("GithubProjectController::addMembersToTeam(..) - adding member: " + member);
+
+                let opts = {
+                    method: 'PUT',
+                    uri: 'https://api.github.com/teams/' + teamId + '/memberships/' + member,
                     headers: {
                         'Authorization': ctx.GITHUB_AUTH_TOKEN,
                         'User-Agent': ctx.GITHUB_USER_NAME,
                         'Accept': 'application/json'
                     },
-                    json: {
-                        name: groupName
-                    }
-                },
-
-                function (error: any, response: any, body: any) {
-                    if (!error && response.statusCode < 400) {
-                        let id = body.id;
-                        Log.info("GithubProjectController::createTeam(..) - success: " + id);
-                        fulfill(id);
-                        // onSuccess(body.id);
-                    } else {
-                        Log.error("GithubProjectController::createTeam(..) - ERROR: " + response);
-                        reject(error);
-                    }
-                    /*
-                     else if (!error && onError) {
-                     onError(response);
-                     }
-                     else if (error) {
-                     console.error(error);
-                     }
-                     */
-                }
-            );
-        });
-    }
-
-    public addTeamToRepo(teamName: string, groupName: string) {
-        let ctx = this;
-
-        return new Promise(function (fulfill, reject) {
-            request.put(
-                {
-                    url: 'https://api.github.com/teams/' + teamName + '/repos/' + ctx.org + '/' + groupName,
-                    headers: {
-                        'Authorization': ctx.GITHUB_AUTH_TOKEN,
-                        'User-Agent': ctx.GITHUB_USER_NAME,
-                        'Accept': 'application/json'
-                    }
-                },
-
-                function (error: any, response: any, body: any) {
-                    if (!error && response.statusCode < 400) {
-                        Log.info("GithubProjectController::addTeamToRepo(..) - success: " + body);
-                        // onSuccess(body);
-                        fulfill(body);
-                    } else {
-                        Log.error("GithubProjectController::addTeamToRepo(..) - ERROR: " + response);
-                        reject(response);
-                    }
-                    /* else if (!error && onError) {
-                     onError(response);
-                     }
-                     else if (error) {
-                     console.error(error);
-                     }
-                     */
-                }
-            );
-        });
-    }
-
-    public addMembersToTeam(members: string[], teamName: string) {
-        let ctx = this;
-
-        return new Promise(function (fulfill, reject) {
-
-            for (var m of members) {
-                var member = m;// m.github_id;
-                if (member) {
-                    request.put(
-                        {
-                            url: 'https://api.github.com/teams/' + teamName + '/memberships/' + member,
-                            headers: {
-                                'Authorization': ctx.GITHUB_AUTH_TOKEN,
-                                'User-Agent': ctx.GITHUB_USER_NAME,
-                                'Accept': 'application/json'
-                            }
-                        },
-
-                        function (error: any, response: any, body: any) {
-                            if (!error && response.statusCode < 400) {
-                                // onSuccess(body);
-                                Log.error("GithubProjectController::addMembersToTeam(..) - success: " + body);
-                                fulfill(body);
-                            } else {
-                                Log.error("GithubProjectController::addMembersToTeam(..) - ERROR: " + response);
-                                reject(response);
-                            }
-                            /*
-                             else if (!error && onError) {
-                             onError(response);
-                             }
-                             else if (error) {
-                             console.error(error);
-                             }
-                             */
-
-                        }
-                    );
-                }
+                    json: true
+                };
+                promises.push(rp(opts));
             }
+
+            Promise.all(promises).then(function (results: any) {
+                Log.info("GithubProjectController::addMembersToTeam(..) - success: " + results);
+                fulfill(results);
+            }).catch(function (err: any) {
+                Log.error("GithubProjectController::addMembersToTeam(..) - ERROR: " + err);
+                reject(err);
+            });
         });
     }
 
---
+++ test/GithubControllerSpec.ts
@@ -0,0 +1,148 @@
+/*
+ import chai = require('chai');
+ import restify = require('restify');
+ import sinon = require('sinon');
+ var http = require('http');
+ */
+
+/**
+ * Created by rtholmes on 2016-08-17.
+ */
+
+
+import MemoryStore from '../src/controller/GithubProjectController';
+import Log from "../src/Util";
+import GithubProjectController from "../src/controller/GithubProjectController";
+
+var expect = require('chai').expect;
+
+
+describe("GithubProjectController", function () {
+
+    var controller: GithubProjectController;
+
+    let REPO_NAME1 = 'MochaTestRepo1';
+    let TEAM_NAME1 = 'MochaTestTeam1';
+
+    let REPO_NAME2 = 'MochaTestRepo2';
+    let TEAM_NAME2 = 'MochaTestTeam2';
+
+    beforeEach(function () {
+        controller = new GithubProjectController();
+    });
+
+    // no assertions because we don't really care about repo deletion
+    it("Should be able to delete a repo", function () {
+        return controller.deleteRepo(REPO_NAME1).then(function (res) {
+            Log.test('Repo deleted: ' + res);
+            // expect(res).not.to.be.null;
+        }).catch(function (err) {
+            Log.test('Did not delete repo: ' + err);
+            // expect(true).to.be.false; // should not happen
+        });
+
+    });
+
+
+    // works the first time (if the project doesn't exist)
+    it("Should be able to create a repo", function () {
+        return controller.createRepo(REPO_NAME1).then(function (res) {
+            Log.test('Repo created: ' + res);
+            expect(res).not.to.be.null;
+        }).catch(function (err) {
+            Log.test('Did not create repo: ' + err);
+            expect(true).to.be.false; // should not happen
+        });
+    });
+
+    /*
+     // no assertions because we don't really care about repo deletion
+     it("Should be able to delete a team", function () {
+     return controller.deleteTeam(TEAM_NAME1).then(function (res) {
+     Log.test('Team deleted: ' + res);
+     expect(res).not.to.be.null;
+     }).catch(function (err) {
+     Log.test('Did not delete team: ' + err);
+     expect(true).to.be.false; // should not happen
+     });
+     });
+     */
+
+    it("Should be able to create a team", function () {
+        return controller.createTeam(TEAM_NAME1).then(function (res) {
+            Log.test('Team created: ' + res);
+            expect(res).not.to.be.null;
+        }).catch(function (err) {
+            Log.test('Did not create team: ' + err);
+            expect(true).to.be.false; // should not happen
+        });
+    });
+
+    it("Should be able to add a team to a repo", function () {
+        let teamId = 2100162;
+        return controller.addTeamToRepo(teamId, REPO_NAME1).then(function (res) {
+            Log.test('Team added to repo: ' + res);
+            expect(res).not.to.be.null;
+        }).catch(function (err) {
+            Log.test('Did not add team to repo: ' + err);
+            expect(true).to.be.false; // should not happen
+        });
+    });
+
+    it("Should be able to add members to a team", function () {
+        let teamId = 2100162;
+        let members = ['rtholmes', 'rthse2'];
+        return controller.addMembersToTeam(teamId, members).then(function (res) {
+            Log.test('Members added to team: ' + JSON.stringify(res));
+            expect(res).not.to.be.null;
+        }).catch(function (err: any) {
+            Log.test('Did not add members to team: ' + err);
+            expect(true).to.be.false; // should not happen
+        });
+    });
+
+
+    /**
+     * Works:
+     *
+     * Make sure the team and repo do not already exist.
+     */
+    it("Should be able to configure a complete repo and team with membership", function () {
+        this.timeout(5000);
+
+        // let teamId = 2100162;
+        let members = ['rtholmes', 'rthse2'];
+
+        let outerTeamId = -1;
+
+        // create repo
+        return controller.createRepo(REPO_NAME2).then(function (res) {
+            Log.test('Repo created: ' + res);
+            expect(res).not.to.be.null;
+
+            // create team
+            return controller.createTeam(TEAM_NAME2);
+        }).then(function (teamId: number) {
+            Log.test('Team created: ' + teamId);
+            outerTeamId = teamId;
+
+            // add members to team
+            return controller.addMembersToTeam(teamId, members);
+        }).then(function (res: any) {
+            Log.test('Members added to team: ' + JSON.stringify(res));
+
+            // add team to repo
+            return controller.addTeamToRepo(outerTeamId, REPO_NAME2);
+        }).then(function (res: any) {
+            Log.test('Team added to repo: ' + res);
+
+            Log.test('Team, Repo, and Membership configured for: ' + REPO_NAME2);
+            expect(true).to.be.true;
+        }).catch(function (err) {
+            Log.test('Something went wrong: ' + err);
+            expect(true).to.be.false; // should not happen
+        });
+
+    });
+
+});
