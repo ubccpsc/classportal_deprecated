837a97952940bd88a7e3f2ac875355f7ff48722e
Michael Sargent <mksarge@users.noreply.github.com>
2016-09-02 11:47:19 -0700

    fixed ubc logo display issue (I think?)

--- frontend/modules/App.js
+++ frontend/modules/App.js
@@ -6,7 +6,7 @@ export default React.createClass({
     return (
       <div>
         <div id="titlebar">
-          <img id="titlebar-img" src="UBCLogo_Reverse.png"/>
+          <img id="titlebar-img" src="../UBCLogo_Reverse.png"/>
           <h1 id="titlebar-text">{config.titlebar} | </h1>
           <h3 id="titlebar-subtext">{config.titlebar_subtext}</h3>
         </div>
