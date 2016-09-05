478d0ef353969727a727c514a57f759e7ddcacae
Michael Sargent <mksarge@users.noreply.github.com>
2016-09-05 02:51:40 -0700

    cleaned up code

--- frontend/modules/admin_portal/AdminPortal.js
+++ frontend/modules/admin_portal/AdminPortal.js
@@ -52,20 +52,17 @@ export default React.createClass({
     this.getClasslist();
   },
   render: function () {
-    //todo: transition to .bind(this);
-    var that = this;
-    
-    //Read more about passing props to this.props.children: http://stackoverflow.com/questions/32370994/how-to-pass-props-to-this-props-children
+    //more info: http://stackoverflow.com/questions/32370994/how-to-pass-props-to-this-props-children
     var childrenWithProps = React.Children.map(this.props.children, function (child) {
       return React.cloneElement(child, {
-        "admin": that.state.adminObject,
-        "students": that.state.studentsFile,
-        "teams": that.state.teamsFile,
-        "deliverables": that.state.deliverablesFile,
-        "grades": that.state.gradesFile,
-        "classlist": that.state.classlist
+        "admin": this.state.adminObject,
+        "students": this.state.studentsFile,
+        "teams": this.state.teamsFile,
+        "deliverables": this.state.deliverablesFile,
+        "grades": this.state.gradesFile,
+        "classlist": this.state.classlist
       });
-    });
+    }.bind(this));
     
     return (
       <div>
@@ -87,79 +84,3 @@ export default React.createClass({
       </div>
     )}
 })
-
-/*
-
-this.getAdmin();
-this.getDeliverables();
-this.getClasslist();
-this.getStudents();
-this.getTeams();
-
-//returns admin object from admins.json
-  getAdmin: function () {
-    Ajax.getAdmin(
-      function success(response) {
-        console.log("AdminPortal.js| Retreived admin file. " + response);
-        this.setState({ adminObject: response });
-      }.bind(this),
-      function error(xhr, status, error) {
-        console.log("AdminPortal.js| Error getting admin!");
-      }.bind(this)
-    )
-  },
-  //returns teams.json
-  getTeams: function () {
-    Ajax.getTeams(
-      function success(response) {
-        console.log("AdminPortal.js| Retreived teams file.");
-        this.setState({ teamsObject: response });
-      }.bind(this),
-      function error(xhr, status, error) {
-        console.log("AdminPortal.js| Error getting teams!");
-      }.bind(this)
-    )
-  },
-  //returns students.json
-  getStudents: function () {
-    Ajax.getStudents(
-      function success(response) {
-        console.log("AdminPortal.js| Retreived students file.");
-        this.setState({ studentsObject: response });
-      }.bind(this),
-      function error(xhr, status, error) {
-        console.log("AdminPortal.js| Error getting students!");
-      }.bind(this)
-    )
-  },
-  //returns deliverables.json
-  getDeliverables: function () {
-    Ajax.getDeliverables(
-      function success (response) {
-        console.log("AdminPortal.js| Retrieved deliverables file.");
-        this.setState({ deliverablesObject: response });
-      }.bind(this),
-      function error (xhr, status, err) {
-        console.log("AdminPortal.js| Error retrieving deliverables!");
-      }.bind(this)
-    )
-  },
-  getClasslist: function () {
-    Ajax.getClasslist(
-      function success(response) {
-        console.log("AdminPortal.js| Retrieved class list:" + response);
-        
-        //convert classlist into format useable by Elemental Form-Select
-        var classlistWithLabels = []
-        for (var index = 0; index < response.length; index++){
-          classlistWithLabels[index] = { "label": response[index] };
-        }
-        
-        this.setState({ classlist: classlistWithLabels });
-      }.bind(this),
-      function error(xhr, status, error) {
-        console.log("AdminPortal.js| Error getting classlist!");
-      }.bind(this)
-    )
-  },
-*/
