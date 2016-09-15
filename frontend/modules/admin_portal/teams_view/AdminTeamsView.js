9633880c6fb4cdb985a59cef7161fb2b61bfc78d
Michael Sargent <michaelksarge@gmail.com>
2016-09-15 13:48:29 -0700

    improved admin teams ui

--- frontend/modules/admin_portal/teams_view/AdminTeamsView.js
+++ frontend/modules/admin_portal/teams_view/AdminTeamsView.js
@@ -7,8 +7,15 @@ export default React.createClass({
   render: function () {
     return (
       <div>
-        <AdminTeams teams={this.props.files.teamsFile} students={this.props.files.studentsFile} myTeams={this.props.files.adminsFile[this.props.files.myAdminIndex].teams} />
-        <CreateTeam namesArray={this.props.files.namesArray} isAdmin="true" studentName="null" />
+        <AdminTeams
+          teams={this.props.files.teamsFile}
+          students={this.props.files.studentsFile}
+          admins={this.props.files.adminsFile}
+          myAdminIndex = {this.props.files.myAdminIndex} />
+        <CreateTeam
+          namesArray={this.props.files.namesArray}
+          isAdmin="true"
+          studentName="null" />
         {this.props.files.adminsFile[this.props.files.myAdminIndex].prof === true && (<CreateProjects />) }
       </div>
     )
