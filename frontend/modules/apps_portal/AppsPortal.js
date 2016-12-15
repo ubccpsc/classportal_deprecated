fe43c14905a729b338a9e0cea36a2cc4df8615f9
marquesarthur <marques.art@gmail.com>
2016-12-15 00:21:27 -0800

    Added config and link to app store

---
+++ frontend/modules/apps_portal/AppsPortal.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Logout from '../shared_components/Logout'
+import CreateTeam from '../shared_components/CreateTeam'
+import Ajax from '../shared_components/Ajax'
+
+export default React.createClass({
+  getInitialState: function () {
+    return {
+      
+    };
+  },
+  render: function () {
+    return (
+      <div>
+        Hello! I'm a shy app store
+      </div>
+    )
+  }
+})
