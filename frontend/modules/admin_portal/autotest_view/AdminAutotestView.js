4d3c283afe5a1275b1f682050a103024627ef4c7
marquesarthur <marques.art@gmail.com>
2017-01-04 11:42:49 -0800

    Fixed warnings, added the autotest tab, and finished comments approval

---
+++ frontend/modules/admin_portal/autotest_view/AdminAutotestView.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ContentModule from '../../shared_components/ContentModule'
+
+
+export default React.createClass({
+  render: function () {
+    return (
+		<ContentModule id="admin-autotest-module" title={"Autotest"} initialHideContent={false}>
+	      <div>
+	        <p>Not yet implemented</p>
+	      </div>
+	    </ContentModule>
+    )
+  }
+})
