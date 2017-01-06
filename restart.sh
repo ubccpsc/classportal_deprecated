73aaa6b4350877f8d3035e32571cad37e90d53a2
Reid Holmes <rtholmes@gmail.com>
2017-01-06 10:24:32 -0800

    update restart to not hard code the port

--- restart.sh
+++ restart.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-port=11410
+port=$(cat config.json | jq -r '.port')
+echo "ClassPortal running on ${port}"
 process_pid=$(netstat -anp tcp | grep $port | grep -Po '\d+\/node' | grep -Po '\d+' | sort | uniq)
 echo "Process ${process_pid} found running on port ${port}"
 kill -9 $process_pid
