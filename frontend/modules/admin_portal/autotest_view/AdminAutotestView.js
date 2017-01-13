259b41804c361cfeb8b6ebb8a210b04dcb0acd50
marquesarthur <marques.art@gmail.com>
2017-01-13 13:50:03 -0800

    Autotest dashboard

--- frontend/modules/admin_portal/autotest_view/AdminAutotestView.js
+++ frontend/modules/admin_portal/autotest_view/AdminAutotestView.js
@@ -1,14 +1,121 @@
-import React from 'react'
-import ContentModule from '../../shared_components/ContentModule'
+import React from 'react';
+import ContentModule from '../../shared_components/ContentModule';
+import data from 'dashboard';
+import Dashboard from './Dashboard'
+import DataParser from './DataParser'
+
+import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+
+import { Form, FormRow, FormField, FormInput, FormIconField, FormSelect, Dropdown,
+    Glyph, Button, ButtonGroup,
+    Row, Col,
+    Modal, ModalHeader, ModalBody, ModalFooter } from 'elemental'
 
 
 export default React.createClass({
+  getInitialState: function () {
+  	var processed_rows = DataParser.process_dashboard_rows(data.rows);
+    return {
+    	rows: processed_rows,
+    	sort: {}
+    }
+  },
+  sortByDate: function () {
+  	var timestamp_index = 10;
+  	var sort = this.state.sort;
+  	if (typeof sort['date'] === 'undefined') {
+  		sort['date'] = { desc: true};
+  	} else {
+		sort['date'].desc = !sort['date'].desc;
+  	}
+
+  	var rows = this.state.rows;
+  	rows.sort(function(a, b){
+  		if (sort['date'].desc) {
+  			return b[timestamp_index] - a[timestamp_index];
+  		} else {
+  			return a[timestamp_index] - b[timestamp_index];
+  		}
+  	});
+  	
+  	this.setState({ sort: sort, rows: rows });
+  },
+  sortBy: function(key, index){
+  	var timestamp_index = 10;
+  	var sort = this.state.sort;
+  	if (typeof sort[key] === 'undefined') {
+  		sort[key] = { desc: true};
+  	} else {
+		sort[key].desc = !sort[key].desc;
+  	}
+
+  	var rows = this.state.rows;
+  	rows.sort(function(a, b){
+  		if (sort[key].desc) {
+  			return b[index] - a[index];
+  		} else {
+  			return a[index] - b[index];
+  		}
+  	});
+  	
+  	this.setState({ sort: sort, rows: rows });
+  },
+  renderDashboard: function() {
+
+  	var autotest_dashboard = this.state.rows.map(function(autotest_data, i) {
+  		var dkey = "dashboard_"+i;
+
+  		return (
+  			<Dashboard 
+  				index={i}
+  				date={autotest_data[0]} 
+  				repo={autotest_data[1]} 
+  				sec={autotest_data[2]}
+  				overall={autotest_data[3]}
+  				pass={autotest_data[4]} 
+  				cover={autotest_data[5]} 
+  				np={autotest_data[6]}
+  				nf={autotest_data[7]}
+  				ns={autotest_data[8]} 
+  				results={autotest_data[9]} 
+  				key={dkey}></Dashboard>
+  		)
+    });
+
+    return autotest_dashboard;
+  },
   render: function () {
+  	
+	var headerLineStyle = { 
+      backgroundColor: "#002145",
+      paddingTop: "5px",
+      paddingBottom: "5px",
+      color: "white"
+    };
+
+    var sortable = {
+    	color: "white"
+    };
+
     return (
 		<ContentModule id="admin-autotest-module" title={"Autotest"} initialHideContent={false}>
-	      <div>
-	        <p>Not yet implemented</p>
-	      </div>
+		<Row style={headerLineStyle}>
+	        <Col sm="10%"><a style={sortable} onClick={this.sortByDate}>Date</a></Col>
+	        <Col sm="20%">Repo</Col>
+	        <Col sm="5%"><a style={sortable} onClick={this.sortBy.bind(this, 'nsec', 2)}>#Sec</a></Col>
+	        <Col sm="7%"><a style={sortable} onClick={this.sortBy.bind(this, 'poverall', 3)}>% overall</a></Col>
+	        <Col sm="7%"><a style={sortable} onClick={this.sortBy.bind(this, 'ppass', 4)}>% pass</a></Col>
+	        <Col sm="8%"><a style={sortable} onClick={this.sortBy.bind(this, 'pcover', 5)}>% cover</a></Col>
+	        <Col sm="5%"><a style={sortable} onClick={this.sortBy.bind(this, 'npass', 6)}>#P</a></Col>
+	        <Col sm="5%"><a style={sortable} onClick={this.sortBy.bind(this, 'nfail', 7)}>#F</a></Col>
+	        <Col sm="5%"><a style={sortable} onClick={this.sortBy.bind(this, 'nskip', 8)}>#S</a></Col>
+	        <Col sm="28%">Results</Col>
+      	</Row>
+	      
+        <div>
+			{this.renderDashboard()}
+			<br></br>
+        </div>
 	    </ContentModule>
     )
   }
